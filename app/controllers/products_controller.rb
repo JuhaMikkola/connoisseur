1c533b050175634e432b4f57b485d6f43b817471
JuhaMikkola <juha@Juhas-MacBook-Pro.local>
2013-11-14 14:13:18 -0500

    Product Show is Working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,11 +3,22 @@ require 'open-uri'
 class ProductsController < ApplicationController
 
   def index
-    products_json = open('http://lcboapi.com/products').read
+    products_json = open('http://lcboapi.com/products?per_page=50').read
     @products = JSON.parse(products_json)
   end
 
-  def view
+  def show
+    id = params[:id]
+    products_json = open('http://lcboapi.com/products?per_page=50').read
+    products = JSON.parse(products_json)
+    products['result'].each do |product|
+      @product=product if product['id'].to_i==id.to_i 
+    end
   end
+
+  # private
+  # def product_params
+  #   params.require(:product).permit(:name, :price_in_cents, :primary_category)
+  # end
 
 end
